8f8c764a32677ff0ba7dbb18e211eccde2b7808d
Lindsey Kopacz <lkopacz@users.noreply.github.com>
2020-11-11 20:16:41 -0500

    Added elapsed time and formatters.

--- src/components/audio-player.js
+++ src/components/audio-player.js
@@ -1,5 +1,23 @@
 import { useRef, useState } from "react";
 
+const formatTime = (time) => {
+  // Hours, minutes and seconds
+  const hrs = Math.floor(~~(time / 3600)); // eslint-disable-line
+  const mins = Math.floor(~~((time % 3600) / 60)); // eslint-disable-line
+  const secs = Math.floor(time % 60);
+
+  // Output like "1:01" or "4:03:59" or "123:03:59"
+  let ret = "";
+
+  if (hrs > 0) {
+    ret += `${hrs}:${mins < 10 ? "0" : ""}`;
+  }
+
+  ret += `${mins}:${secs < 10 ? "0" : ""}`;
+  ret += `${secs}`;
+  return ret;
+};
+
 const AudioPlayer = ({ src, transcript }) => {
   // Create a play button that toggles play and pause
   // Create elapsed time and total time
@@ -12,6 +30,7 @@ const AudioPlayer = ({ src, transcript }) => {
 
   const [isPlaying, setIsPlaying] = useState(false);
   const [duration, setDuration] = useState(0);
+  const [mediaTime, setMediaTime] = useState(0);
 
   const togglePlaying = () => {
     setIsPlaying(!isPlaying);
@@ -22,16 +41,21 @@ const AudioPlayer = ({ src, transcript }) => {
     setDuration(audioRef.current.duration);
   };
 
+  const onTimeUpdate = () => {
+    setMediaTime(audioRef.current.currentTime);
+  };
+
   return (
     <>
       <div className="audio">
         <button onClick={togglePlaying}>{isPlaying ? "Pause" : "Play"}</button>
-        <span className="elapsed">Elapsed Time: 0</span>
-        <span className="duration">Total Time: {duration}</span>
+        <span className="elapsed">Elapsed Time: {formatTime(mediaTime)}</span>
+        <span className="duration">Total Time: {formatTime(duration)}</span>
       </div>
       <audio
         ref={audioRef}
         onLoadedMetadata={onLoadedMetadata}
+        onTimeUpdate={onTimeUpdate}
         src={src}
         controls
       />
