db75073d4c64b950bbad66d5d85dc6d344b0ef43
Lindsey Kopacz <lkopacz@users.noreply.github.com>
2020-11-11 20:10:15 -0500

    Added toggle play.

--- src/components/audio-player.js
+++ src/components/audio-player.js
@@ -1,3 +1,5 @@
+import { useRef, useState } from "react";
+
 const AudioPlayer = ({ src, transcript }) => {
   // Create a play button that toggles play and pause
   // Create elapsed time and total time
@@ -6,9 +8,21 @@ const AudioPlayer = ({ src, transcript }) => {
   // create a playback rate button
   // volume slider
   // mute toggle
+  const audioRef = useRef(null);
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlaying = () => {
+    setIsPlaying(!isPlaying);
+    isPlaying ? audioRef.current.pause() : audioRef.current.play();
+  };
+
   return (
     <>
-      <audio src={src} controls />
+      <div>
+        <button onClick={togglePlaying}>{isPlaying ? "Pause" : "Play"}</button>
+      </div>
+      <audio ref={audioRef} src={src} controls />
       <div>{transcript}</div>
     </>
   );
